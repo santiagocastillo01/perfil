0119cdb560fd4dc4e1d705bc6c1d0727dba8723f
santiagocastillo01 <castillosantiago441@gmail.com>
2024-04-01 20:06:25 -0300

    portfolio

--- app.js
+++ app.js
@@ -12,8 +12,10 @@ app.set('views', path.join(__dirname, 'views'));
 // Middleware para servir archivos estáticos desde la carpeta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rutas para renderizar las vistas EJS
+// Si tus archivos CSS están en una carpeta 'css' dentro de 'public'
+app.use('/css', express.static(path.join(__dirname, 'public', 'css')));
 
+// Rutas para renderizar las vistas EJS
 app.get('/contacto', (req, res) => {
     res.render('contacto');
 });
@@ -25,6 +27,7 @@ app.get('/perfil', (req, res) => {
 app.get('/', (req, res) => {
     res.render('home');
 });
+
 // Iniciar el servidor
 app.listen(process.env.PORT, () => {
     console.log("Servidor en Puerto " + process.env.PORT + " | http://localhost:" + process.env.PORT);
